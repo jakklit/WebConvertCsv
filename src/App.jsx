c5a21f440c92f4fe5b91d344e4976965b1b0c487
jakklit <108712806+jakklit@users.noreply.github.com>
2024-10-16 10:07:37 +0700

    add react router dom for multi pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,16 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import ConvertFile from './pages/ConvertFile';
 
 function App() {
-  const [selectedFiles, setSelectedFiles] = useState([]);
-
-  // ฟังก์ชันในการจัดการการเลือกไฟล์
-  const handleFileChange = (event) => {
-    setSelectedFiles(event.target.files); 
-  };
-
-  const handleFileUpload = async () => {
-    if (selectedFiles.length === 0) {
-      alert('กรุณาเลือกไฟล์');
-      return;
-    }
-
-    const formData = new FormData();
-    for (let i = 0; i < selectedFiles.length; i++) {
-      formData.append('csvFiles', selectedFiles[i]); 
-    }
-
-    try {
-      const response = await fetch('https://convertfilecsv.onrender.com/api/upload', {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        const result = await response.blob(); // รับ response เป็น blob
-
-        // ตรวจสอบชื่อไฟล์ที่ถูกอัปโหลด
-        let filename = '';
-        if (selectedFiles.length > 0) {
-          const firstFile = selectedFiles[0].name;
-
-          const match = firstFile.match(/(\d{6})_(ELOOP[A-Z]+)_File/);
-          if (match) {
-            filename = `${match[1]}_${match[2]}_File.csv`; 
-          } else {
-            filename = 'merged_output.csv'; 
-          }
-        }
-        const url = window.URL.createObjectURL(result);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = filename; 
-        a.click(); 
-      } else {
-        console.error('Upload failed');
-      }
-    } catch (error) {
-      console.error('Error uploading files:', error);
-    }
-  };
-
   return (
-    <>
-      <div>
-        <input type="file" multiple onChange={handleFileChange} />
-        <button onClick={handleFileUpload}>Upload Files</button>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ConvertFile />} />
+      </Routes>
+    </Router>
   );
+
 }
 
 export default App;
---
+++ src/pages/ConvertFile.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+function ConvertFile() {
+    const [selectedFiles, setSelectedFiles] = useState([]);
+
+    const handleFileChange = (event) => {
+        setSelectedFiles(event.target.files);
+    };
+
+    const handleFileUpload = async () => {
+        if (selectedFiles.length === 0) {
+            alert('กรุณาเลือกไฟล์');
+            return;
+        }
+
+        const formData = new FormData();
+        for (let i = 0; i < selectedFiles.length; i++) {
+            formData.append('csvFiles', selectedFiles[i]);
+        }
+
+        try {
+            const response = await fetch('http://localhost:3000/api/upload', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.ok) {
+                const result = await response.blob();
+
+                let filename = '';
+                if (selectedFiles.length > 0) {
+                    const firstFile = selectedFiles[0].name;
+
+                    const match = firstFile.match(/(\d{6})_(ELOOP[A-Z]+)_File/);
+                    if (match) {
+                        filename = `${match[1]}_${match[2]}_File.csv`;
+                    } else {
+                        filename = 'merged_output.csv';
+                    }
+                }
+                const url = window.URL.createObjectURL(result);
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = filename;
+                a.click();
+            } else {
+                console.error('Upload failed');
+            }
+        } catch (error) {
+            console.error('Error uploading files:', error);
+        }
+    };
+
+    return (
+        <>
+            <div>
+                <input type="file" multiple onChange={handleFileChange} />
+                <button onClick={handleFileUpload}>Upload Files</button>
+            </div>
+        </>
+    );
+}
+
+export default ConvertFile
